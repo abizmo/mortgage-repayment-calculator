b105320422f569b5ef2dd2ee79fef50107c8cd29
abizmo <8437730+abizmo@users.noreply.github.com>
2025-04-27 19:55:10 +0100

    feat: improve styling and layout of RadioGroup and RadioGroupItem components

--- src/components/mortgage-calculator/components/mortgage-form.tsx
+++ src/components/mortgage-calculator/components/mortgage-form.tsx
@@ -177,7 +177,7 @@ function MortgageForm({ onCalculate, onReset }: MortgageFormProps) {
           control={form.control}
           name="type"
           render={({ field }) => (
-            <FormItem className="space-y-3">
+            <FormItem className="space-y-3 gap-4">
               <FormLabel>Mortgage Type</FormLabel>
               <FormControl>
                 <RadioGroup
@@ -187,19 +187,29 @@ function MortgageForm({ onCalculate, onReset }: MortgageFormProps) {
                     field.onChange(value);
                   }}
                   // defaultValue={field.value}
-                  className="flex flex-col space-y-1"
+                  className="flex flex-col"
                 >
-                  <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormItem className="group has-checked:border-primary has-checked:bg-primary/15 flex items-center space-x-4 space-y-0 border border-guide-500 p-4 rounded-md">
                     <FormControl>
-                      <RadioGroupItem value="repayment" />
+                      <RadioGroupItem
+                        value="repayment"
+                        className="group-has-checked:border-primary"
+                      />
                     </FormControl>
-                    <FormLabel className="font-normal">Repayment</FormLabel>
+                    <FormLabel className="text-guide-900 font-bold">
+                      Repayment
+                    </FormLabel>
                   </FormItem>
-                  <FormItem className="flex items-center space-x-3 space-y-0">
+                  <FormItem className="group has-checked:border-primary has-checked:bg-primary/15 flex items-center space-x-4 space-y-0 border border-guide-500 p-4 rounded-md">
                     <FormControl>
-                      <RadioGroupItem value="interest-only" />
+                      <RadioGroupItem
+                        value="interest-only"
+                        className="group-has-checked:border-primary"
+                      />
                     </FormControl>
-                    <FormLabel className="font-normal">Interest Only</FormLabel>
+                    <FormLabel className="text-guide-900 font-bold">
+                      Interest Only
+                    </FormLabel>
                   </FormItem>
                 </RadioGroup>
               </FormControl>
